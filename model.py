1d8422986145bd4031d1aec8b95ee602e1a66d29
Jasl-hub <165571472+Jasl-hub@users.noreply.github.com>
2025-08-08 17:36:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -87,6 +87,7 @@ import torch
 from datetime import datetime
 from scipy.stats import spearmanr
 
+
 spacy_lg = spacy.load("en_core_web_md")
 spacy_sm = spacy.load("en_core_web_sm")
 tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
